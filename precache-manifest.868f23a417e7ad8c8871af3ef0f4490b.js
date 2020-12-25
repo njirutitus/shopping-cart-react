c4197ce5001c44bab38691e30291145c9b230275
titus njiru <njirutitus@gmail.com>
2020-12-25 22:09:29 +0300

    Updates

---
+++ precache-manifest.868f23a417e7ad8c8871af3ef0f4490b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108049d39aed340e56bcc34eb40cdda8",
+    "url": "/shopping-cart-react/index.html"
+  },
+  {
+    "revision": "21b6967ac0489878a263",
+    "url": "/shopping-cart-react/static/js/2.1f7b6fcf.chunk.js"
+  },
+  {
+    "revision": "987e92042e5611be7ae4",
+    "url": "/shopping-cart-react/static/js/main.44ea6fc2.chunk.js"
+  },
+  {
+    "revision": "218f8eb1b92a1be609cd",
+    "url": "/shopping-cart-react/static/js/runtime~main.36ec2802.js"
+  }
+]);
